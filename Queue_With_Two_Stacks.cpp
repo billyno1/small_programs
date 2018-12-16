d19cb0d85ea83796fc0e7b63f241cec31cd2499d
billyno1 <billyvampire@hotmail.com>
2018-12-15 23:06:21 -0500

    Update Queue_With_Two_Stacks.cpp

--- Queue_With_Two_Stacks.cpp
+++ Queue_With_Two_Stacks.cpp
@@ -9,35 +9,49 @@ using namespace std;
 class Queue {
 	stack<char> stack1;
 	stack<char> stack2;
+	string lastOperation;
 	
 public:
-	void push(char ch) {
-		if(stack1.size() == 0) {
-			stack2.push(ch);
-		} else {
-			stack1.push(ch);
-		}
-	}
-
-	char pop() {
+	void switchStack() {
 	    if(stack1.size() == 0) {
         	while(stack2.size() != 0) {
         		stack1.push(stack2.top());
         		stack2.pop();
-        	}	    
-        	char retval = stack1.top();
-        	stack1.pop();
-        	return retval;
+        	}	 
         } else {
         	while(stack1.size() != 0) {
         		stack2.push(stack1.top());
         		stack1.pop();
         	}	 
-        	char retval = stack2.top();
-        	stack2.pop();
-        	return retval;
         }  
+	}		
 
+	void push(char ch) {
+		if(lastOperation == "pop") {
+			switchStack();
+		}
+		if(stack1.size() == 0) {
+			stack2.push(ch);
+		} else {
+			stack1.push(ch);
+		}
+		lastOperation = "push";
+	}
+
+	char pop() {
+	    char retval;
+		if(lastOperation == "push") {
+			switchStack();
+		}
+	    if(stack2.size() == 0) {
+        	retval = stack1.top();
+        	stack1.pop();
+        } else {
+        	retval = stack2.top();
+        	stack2.pop();
+        }  
+        lastOperation = "pop";
+        	return retval;
 	}
 };
 
@@ -52,10 +66,18 @@ int main() {
 	for(int i = 0; i < a.size(); i++) {
 		b += queue.pop();
 	}
+	assert  (!a.compare(b));
+	
+	b = "";
+	queue.push('1');
+	b += queue.pop();
+	queue.push('2');
+	b += queue.pop();
+	queue.push('3');
+	queue.push('4');
+	b += queue.pop();
+	b += queue.pop();
 	cout << a << endl;
 	cout << b << endl;
-	assert  (!a.compare(b));
+	assert  (b == "1234");
 }
-
-
-
